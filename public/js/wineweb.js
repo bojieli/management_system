36630257f3a6cbd408414fd96207e3e6243e658e
cdd <cddlyf@gmail.com>
2014-08-11 12:29:01 +0800

    修复了前台的bug

--- public/js/wineweb.js
+++ public/js/wineweb.js
@@ -40,11 +40,13 @@ $(function(){
   // });
 
   function orderProcess(method){
+    $('span.inputrequired,span.formaterror').hide();
+
     var orderID = $('span#orderID').text();
     var modifyinfo = {
         address: {
-          area: $("select#area option:selected").text(),
-          detail: $("#order_address_detail").text(),
+          area: $("select#order_address_area option:selected").text(),
+          detail: $("#order_address_detail").val(),
           name: $("#order_username").val(),
           tel: $("#order_usertel").val()
         },
@@ -53,35 +55,60 @@ $(function(){
                         + $('textarea#unprocessorder_delete_note').val()
       };
 
-    var alertString = "";
+    var satify = true;
 
-    if(!usernameVertify(modifyinfo.address.name)){
-      alertString = alertString + "用户名不能为空,且长度应小于5\n";
-      $('input#order_username').val("");
+    nameverify = usernameVertify(modifyinfo.address.name);
+    telverify = usertelVertify(modifyinfo.address.tel);
+    areaverify = addressAreaVertify(modifyinfo.address.area);
+    detailverify = addressDetailVertify(modifyinfo.address.detail);
+    noteverify = noteVertify(modifyinfo.notes);
+    dispatchverify = dispatchCenterVertify(modifyinfo.dispatchCenter);
+
+    if(nameverify != 0){
+      satify = false;
+      if(nameverify == -1){
+         $('input#order_username').nextAll('span.inputrequired').show();
+      }else if(nameverify == 1){
+       $('input#order_username').nextAll('span.formaterror').show();
+      }
     }
-    if(!usertelVertify(modifyinfo.address.tel)){
-      alertString = alertString + "联系电话不能为空或格式不正确\n";
-      $('input#order_usertel').val("");
+    if(telverify != 0){
+      satify = false;
+      if(telverify == -1){
+         $('input#order_usertel').nextAll('span.inputrequired').show();
+      }else if(telverify == 1){
+       $('input#order_usertel').nextAll('span.formaterror').show();
+      }
     }
-    if(!addressDetailVertify(modifyinfo.address.detail)){
-      alertString = alertString + "地址详情不能为空,且长度应小于50\n";
-      $('textarea#order_address_detail').val("");
+    if(areaverify != 0){
+      satify = false;
+      $('select#order_address_area').nextAll('span.inputrequired').show();
     }
-    if(!noteVertify(modifyinfo.notes)){
-       alertString = alertString + "备注信息长度应小于50\n";
+    if(detailverify != 0){
+      satify = false;
+      if(detailverify == -1){
+        $('textarea#order_address_detail').nextAll('span.inputrequired').show();
+      }else if(detailverify == 1){
+        $('textarea#order_address_detail').nextAll('span.formaterror').show();
+      }
+    }
+    if(noteverify != 0){
+      satify = false;
+      $('textarea#order_note').siblings('span.formaterror').show();
+    }
+    if(dispatchverify != 0){
+      satify = false;
+      $('select#order_dispatch').siblings('span.inputrequired').show();
+    }
+    /*if(!noteVertify(modifyinfo.notes)){
       $('textarea#order_note').val("");
       $('textarea#unprocessorder_delete_note').val("");
       if($("input[name = 'reasonoRadios']:checked",'div#reason_radios').length != 0){
         $("input[name = 'reasonoRadios']:checked",'div#reason_radios').attr('checked',false);
       }
-    }
-
-    if(!dispatchCenterVertify(modifyinfo.dispatchCenter)){
-      alertString = alertString + "请选择快递点!\n";
-    }
-
-    if(alertString.length > 0){
-      alert(alertString);
+    }*/
+    if(!satify){
+      alert("提交的内容不符合条件！");
       return;
     }else{
       var postData = {
@@ -114,62 +141,89 @@ $(function(){
 /*==========================unprocessed end==================*/
 
 /*========================neworder begin=====================*/
-  $('button#order_wine_add').click(function(){
-    var insert_tr = $('tr.order_wineinfo').first().clone();
-    insert_tr.find('option.hidden_option').attr('selected','selected').end().find('input.winenumber').val("");
-    $('tbody.all_wineinfo').append(insert_tr);
-    $('tbody.all_wineinfo').children().last().find('select.winedescribe').focus();
-  });
-  /*new order*/
-  $("button#neworder_confirm").click(function(){
+  function createOrder(){
+    $('span.inputrequired,span.formaterror').hide();
     var username =   $('input#order_username').val();
     var usertel = $('input#order_usertel').val();
     var area = $('select#order_address_area option:selected').text();
     var detail = $('textarea#order_address_detail').val();
     var notes = $('textarea#order_note').val();
     var dispatchCenter = $('select#order_dispatch option:selected').text();
-    var alertString = "";
-    //begin vertify
-    if(!usernameVertify(username)){
-      alertString = alertString + "用户名不能为空,且长度应小于5\n";
-      $('input#order_username').val("");
-    }
-    if(!usertelVertify(usertel)){
-      alertString = alertString + "联系电话不能为空或格式不正确\n";
-      $('input#order_usertel').val("");
-    }
-    if(!addressDetailVertify(detail)){
-      alertString = alertString + "地址详情不能为空,且长度应小于50\n";
-      $('textarea#order_address_detail').val("");
-    }
-    if(!noteVertify(notes)){
-       alertString = alertString + "备注信息长度应小于50\n";
-      $('textarea#order_note').val("");
-    }
 
     var wines =[];
     var winerows = $('tr.order_wineinfo');
     var wineinfo_right = true;
     for(var i = 0;i < winerows.length;i++){
-      alert('wineinfo'+$(winerows[i]).find('input.winenumber').val());
-      var wineinfo = {
-        id : $(winerows[i]).find('select.winedescribe option:selected').val(),
-        wechatPrice : Number($(winerows[i]).find('select.winewechatprice option:selected').val()),
-        number : Number($(winerows[i]).find('input.winenumber').val())
-      };
-      if(!wineinfoVertify(wineinfo)){
-        if(wineinfo_right){
-         alertString = alertString + "酒的数量最少为1,且为整数\n";
-        }
-        wineinfo_right = false;
-        $(winerows[i]).find('input.winenumber').val("");
-      }else{
-          wines.push(wineinfo);
+      if($(winerows[i]).find('select.winedescribe option:selected').val().length != 0){
+        var wineinfo = {
+          id : $(winerows[i]).find('select.winedescribe option:selected').val(),
+          wechatPrice : Number($(winerows[i]).find('select.winewechatprice option:selected').val()),
+          number : Number($(winerows[i]).find('input.winenumber').val())
+        };
+
+        wines.push(wineinfo);
       }
     }
 
-    if(alertString.length > 0){
-      alert(alertString);
+    //begin vertify
+    var satify = true;
+
+    nameverify = usernameVertify(username);
+    telverify = usertelVertify(usertel);
+    areaverify = addressAreaVertify(area);
+    detailverify = addressDetailVertify(detail);
+    noteverify = noteVertify(notes);
+    dispatchverify = dispatchCenterVertify(dispatchCenter);
+    winesverify = winesInfoVerify(wines);
+
+    if(nameverify != 0){
+      satify = false;
+      if(nameverify == -1){
+        $('input#order_username').nextAll('span.inputrequired').show();
+      }else if(nameverify == 1){
+        $('input#order_username').nextAll('span.formaterror').show();
+      }
+    }
+    if(telverify != 0){
+      satify = false;
+      if(telverify == -1){
+        $('input#order_usertel').nextAll('span.inputrequired').show();
+      }else if(telverify == 1){
+        $('input#order_usertel').nextAll('span.formaterror').show();
+      }
+    }
+    if(areaverify != 0){
+      satify = false;
+      $('select#order_address_area').nextAll('span.inputrequired').show();
+    }
+    if(detailverify != 0){
+      satify = false;
+      if(detailverify == -1){
+        $('textarea#order_address_detail').nextAll('span.inputrequired').show();
+      }else if(detailverify == 1){
+        $('textarea#order_address_detail').nextAll('span.formaterror').show();
+      }
+    }
+    if(noteverify != 0){
+      satify = false;
+      $('textarea#order_note').siblings('span.formaterror').show();
+    }
+    if(dispatchverify != 0){
+      satify = false;
+      $('select#order_dispatch').siblings('span.inputrequired').show();
+    }
+    if(winesverify != 0){
+      satify = false;
+      if(winesverify == -1){
+        $('li#wineinfo_head>span.inputrequired').show();
+      }else{
+        $('li#wineinfo_head>span.formaterror').show();
+      }
+    }
+
+
+    if(!satify){
+      alert("提交订单内容不满足条件！");
       return;
     }else{
       var postData = {
@@ -193,7 +247,17 @@ $(function(){
       }
     });
   }
-});
+  }
+
+  $('button#order_wine_add').click(function(){
+    var insert_tr = $('tr.order_wineinfo').first().clone();
+    insert_tr.find('option.hidden_option').attr('selected','selected').end().find('input.winenumber').val("");
+    $('tbody.all_wineinfo').append(insert_tr);
+    $('tbody.all_wineinfo').children().last().find('select.winedescribe').focus();
+  });
+
+
+  $("button#neworder_confirm").click(createOrder);
 
 
   $('input#username').change(function(){
@@ -239,31 +303,42 @@ $('button#searchorder_search').click(function(){
 
 /*=============vertifymethod begin=======================*/
  function usernameVertify(username){
-    return username.length > 0 && username.length <= username_maxLength;
+    return username.length == 0 ? -1 :(username.length <= username_maxLength ? 0 : 1);
   }
 
   function usertelVertify(usertel){
     var phoneregex = /^0?(13[0-9]|15[012356789]|18[0236789]|14[57])[0-9]{8}$/;
     var landlineregex = /^(\d{3}-?\d{7,8})|(\d{4}-?\d{7,8})$/;
     var landlineregex_noprex = /^\d{7,8}$/;
-    return phoneregex.test(usertel) || landlineregex.test(usertel)||landlineregex_noprex.test(usertel);
+    return usertel.length == 0 ? -1:(phoneregex.test(usertel) || landlineregex.test(usertel)||landlineregex_noprex.test(usertel) ? 0 :1);
   }
 
   function addressDetailVertify(detail){
-    return detail.length > 0 && detail.length <= addressDetail_maxLength ;
+    return detail.length == 0 ? -1 :(detail.length <= addressDetail_maxLength ? 0 : 1);
+  }
+
+  function addressAreaVertify(area){
+    return area.length == 0 ? -1 : 0;
   }
 
   function dispatchCenterVertify(dispatchCenter){
-    return dispatchCenter.length > 0;
+    return dispatchCenter.length == 0 ? -1 : 0;
   }
 
   function noteVertify(notes){
-    return notes.length <= notes_maxLength;
+    return notes.length > notes_maxLength ? -1 : 0;
   }
 
-  function wineinfoVertify(wineinfo){
-    return !isNaN(wineinfo.number) && Number(wineinfo.number) % 1 === 0
-           && Number(wineinfo.number) >= 1 && isFinite(Number(wineinfo.number));
+  function winesInfoVerify(wines){
+    if(wines.length == 0){
+      return -1;
+    }
+    for(var i = 0;i < wines.length;i++){
+      if(isNaN(wines[i].number) || Number(wines[i].number)%1 !== 0 || Number(wines[i].number) <1){
+        return 1;
+      }
+    }
+    return 0;
   }
 
   function searchorderInputVertify(method,inputnumber){
