e972b448add8ba47ccc96f8e4938beaf6cb3bbb2
zhangwei <pb203097@mail.ustc.edu.cn>
2014-08-25 18:37:53 +0800

    git update: kill any process on the listening port

--- tools/git-post-update.sh
+++ tools/git-post-update.sh
@@ -2,5 +2,10 @@
 
 cd $(dirname $0)
 git pull origin master
+
+port=8001
+pid=$(netstat -lntp 2>/dev/null | awk "{if(\$4==\"0.0.0.0:$port\")print \$7}" | awk 'BEGIN{FS="/"}{print $1}')
+[ ! -z "$pid" ] && echo "killing pid $pid on port $port" && kill $pid
 pkill -f management-runner
+
 nohup ./management-runner.sh >/dev/null 2>&1 &
--- tools/management-runner.sh
+++ tools/management-runner.sh
@@ -4,5 +4,5 @@ cd $(dirname $0)/..
 while true; do
 	echo "Starting app..." >>log/app.log
 	node app.js >>log/app.log 2>&1
-	sleep 1
+	sleep 2
 done
