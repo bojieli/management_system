92699e93fcd5562e556beba9697e1b8277cbbb82
zhangwei <pb203097@mail.ustc.edu.cn>
2014-08-29 12:46:21 +0800

    add begin and end timestamp in git-post-update

--- tools/git-post-update.sh
+++ tools/git-post-update.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+echo -n "==== BEGIN "
+date
+
 cd $(dirname $0)
 git pull origin master
 
-port=8001
+port=6088
 pid=$(netstat -lntp 2>/dev/null | awk "{if(\$4==\"0.0.0.0:$port\")print \$7}" | awk 'BEGIN{FS="/"}{print $1}')
 [ ! -z "$pid" ] && echo "killing pid $pid on port $port" && kill $pid
-pkill -f management-runner
+pkill -f app-runner
 
-nohup ./management-runner.sh >/dev/null 2>&1 &
+nohup ./app-runner.sh >/dev/null 2>&1 &
+
+echo -n "===== END "
+date
