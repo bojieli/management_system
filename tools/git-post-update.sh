1c8860c387f03c46646d9217114d7c4e92961ab9
zhangwei <pb203097@mail.ustc.edu.cn>
2014-09-12 22:53:06 +0800

    Revert "add begin and end timestamp in git-post-update"
    This reverts commit 92699e93fcd5562e556beba9697e1b8277cbbb82.

--- tools/git-post-update.sh
+++ tools/git-post-update.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 
-echo -n "==== BEGIN "
-date
-
 cd $(dirname $0)
 git pull origin master
 
-port=6088
+port=8001
 pid=$(netstat -lntp 2>/dev/null | awk "{if(\$4==\"0.0.0.0:$port\")print \$7}" | awk 'BEGIN{FS="/"}{print $1}')
 [ ! -z "$pid" ] && echo "killing pid $pid on port $port" && kill $pid
-pkill -f app-runner
+pkill -f management-runner
 
-nohup ./app-runner.sh >/dev/null 2>&1 &
-
-echo -n "===== END "
-date
+nohup ./management-runner.sh >/dev/null 2>&1 &
